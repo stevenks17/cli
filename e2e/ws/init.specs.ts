173c64b2fa136abec506d8786a2c4b510500c53d
Alfred Cepeda <alfredjcepeda@gmail.com>
2021-11-15 23:57:40 -0500

    Hits on output

--- e2e/ws/init.specs.ts
+++ e2e/ws/init.specs.ts
@@ -1,15 +1,34 @@
 import {run} from "@vlegm/util";
-import {spawn} from "child_process";
+import {ChildProcess, spawn} from "child_process";
 import mockStdin from "mock-stdin";
 const stdin = mockStdin.stdin();
+
+class User {
+  constructor(
+    public child:ChildProcess
+  ) {
+    child.stdout.pipe(process.stdout);
+    child.stdout.on('data', (data) => {
+      if(data.toString().includes('Have a great day!')) {
+        console.log('HIT!');
+      }
+    });
+  }
+}
+function send(child:ChildProcess, chunk:string) {
+
+}
 
 const delay = (ms:number = 1000) => new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
 
 describe('ws: init', () => {
   it('should initialize workstation', async function () {
     this.timeout(0);
-    const child = spawn('yarn', ['cli', 'ws', 'init', 'tmp-test']);
-    child.stdout.pipe(process.stdout);
+    const child = spawn('yarn', ['cli', 'ws', 'init', 'tmp-test'], {
+      stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    const user = new User(child);
 
     await delay();
     child.stdin.write('n');
