7a8914760cc0250cda6e62ec644025e80e3de011
Alfred Cepeda <alfredjcepeda@gmail.com>
2021-11-15 23:41:31 -0500

    Able to create workstation

--- e2e/ws/init.specs.ts
+++ e2e/ws/init.specs.ts
@@ -3,17 +3,32 @@ import {spawn} from "child_process";
 import mockStdin from "mock-stdin";
 const stdin = mockStdin.stdin();
 
-const delay = (ms:number) => new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
+const delay = (ms:number = 1000) => new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
 
 describe('ws: init', () => {
   it('should initialize workstation', async function () {
     this.timeout(0);
-    const child = spawn('yarn', ['cli', 'ws', 'init', 'e2e-test']);
-
+    const child = spawn('yarn', ['cli', 'ws', 'init', 'tmp-test']);
     child.stdout.pipe(process.stdout);
+
+    await delay();
     child.stdin.write('n');
     child.stdin.write('\x0D');
-
-    child.kill();
+    await delay();
+    child.stdin.write('n');
+    child.stdin.write('\x0D');
+    await delay();
+    child.stdin.write('\x0D');
+    await delay();
+    child.stdin.write('n');
+    child.stdin.write('\x0D');
+    await delay();
+    child.stdin.write('n');
+    child.stdin.write('\x0D');
+    await delay();
+    child.stdin.write('y');
+    child.stdin.write('\x0D');
+    await delay();
+    //child.kill();
   });
 });
