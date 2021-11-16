6fdf7dbaa1426563f0f5e9a86fe2b5f664328685
Alfred Cepeda <alfredjcepeda@gmail.com>
2021-11-15 23:27:02 -0500

    Able to run command and send input

--- e2e/ws/init.specs.ts
+++ e2e/ws/init.specs.ts
@@ -1,7 +1,19 @@
-import { run } from "@vlegm/util";
+import {run} from "@vlegm/util";
+import {spawn} from "child_process";
+import mockStdin from "mock-stdin";
+const stdin = mockStdin.stdin();
+
+const delay = (ms:number) => new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
 
 describe('ws: init', () => {
-  it('should initialize workstation', async () => {
-    await run('yarn', ['cli']);
+  it('should initialize workstation', async function () {
+    this.timeout(0);
+    const child = spawn('yarn', ['cli', 'ws', 'init', 'e2e-test']);
+
+    child.stdout.pipe(process.stdout);
+    child.stdin.write('n');
+    child.stdin.write('\x0D');
+
+    child.kill();
   });
 });
