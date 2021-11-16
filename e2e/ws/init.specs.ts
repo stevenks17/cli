53c281be6cfb837fa3cb9c380ad79dfdf268b270
Alfred Cepeda <alfredjcepeda@gmail.com>
2021-11-16 01:15:27 -0500

    Sends input based on question

--- e2e/ws/init.specs.ts
+++ e2e/ws/init.specs.ts
@@ -1,25 +1,66 @@
-import {run} from "@vlegm/util";
 import {ChildProcess, spawn} from "child_process";
-import mockStdin from "mock-stdin";
-const stdin = mockStdin.stdin();
 
 class User {
+  waitingFor?:string;
+  willSend?:string[];
+  resolve?:Function;
+  prompt?:string;
+  sentInput: boolean = false;
+  lastOutput?: string;
+
   constructor(
     public child:ChildProcess
   ) {
     child.stdout.pipe(process.stdout);
-    child.stdout.on('data', (data) => {
-      if(data.toString().includes('Have a great day!')) {
-        console.log('HIT!');
+    child.stdout.on('data', (data:Buffer) => {
+      if(!this.resolve) {
+        return;
+      }
+
+      const output = data.toString('utf-8');
+      if(output.includes(this.prompt) && !this.sentInput) {
+        return this.doSend();
+      }
+
+      if(output.includes('?') && !output.includes(this.prompt)) {
+        this.lastOutput = output;
+        this.resolveSend();
       }
     });
   }
-}
-function send(child:ChildProcess, chunk:string) {
 
-}
+  private resolveSend() {
+    this.resolve();
+    this.resolve = null;
+    this.waitingFor = null;
+    this.sentInput = false;
+    this.prompt = null;
+    this.willSend = null
+  }
 
-const delay = (ms:number = 1000) => new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
+  private doSend() {
+    this.sentInput = true;
+    this.willSend.forEach((chunk) => this.child.stdin.write(chunk));
+  }
+
+  send(prompt: string, chunks:string[], message?:string) {
+    this.prompt = prompt;
+    this.willSend = chunks;
+    this.waitingFor = message;
+
+    if(this.lastOutput && this.lastOutput.includes(this.prompt)) {
+      this.doSend();
+    }
+
+    return new Promise((resolve) => this.resolve = resolve);
+  }
+
+  waitTillDone() {
+    return new Promise<void>((resolve) => {
+      this.child.on('exit', () => resolve());
+    })
+  }
+}
 
 describe('ws: init', () => {
   it('should initialize workstation', async function () {
@@ -30,24 +71,12 @@ describe('ws: init', () => {
 
     const user = new User(child);
 
-    await delay();
-    child.stdin.write('n');
-    child.stdin.write('\x0D');
-    await delay();
-    child.stdin.write('n');
-    child.stdin.write('\x0D');
-    await delay();
-    child.stdin.write('\x0D');
-    await delay();
-    child.stdin.write('n');
-    child.stdin.write('\x0D');
-    await delay();
-    child.stdin.write('n');
-    child.stdin.write('\x0D');
-    await delay();
-    child.stdin.write('y');
-    child.stdin.write('\x0D');
-    await delay();
-    //child.kill();
+    await user.send('Use a config file?', ['n', '\x0D']);
+    await user.send('Add git repos?', ['n', '\x0D']);
+    await user.send('Predefined Services:', ['\x0D']);
+    await user.send('Add environment variables?', ['n', '\x0D']);
+    await user.send('Print workstation config?', ['n', '\x0D']);
+    await user.send('Create your workstation?', ['n', '\x0D']);
+    await user.waitTillDone();
   });
 });
